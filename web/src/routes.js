a54584a07123df082ec48b3c70361710b0c03e2c
zeppi <jessopb@gmail.com>
2021-03-22 18:25:28 -0400

    wip

--- web/src/routes.js
+++ web/src/routes.js
@@ -2,6 +2,8 @@ const { getHtml } = require('./html');
 const { generateStreamUrl } = require('../../ui/util/web');
 const fetch = require('node-fetch');
 const Router = require('@koa/router');
+const fs = require('fs');
+const path = require('path');
 
 // So any code from 'lbry-redux'/'lbryinc' that uses `fetch` can be run on the server
 global.fetch = fetch;
@@ -15,18 +17,27 @@ function getStreamUrl(ctx) {
   return streamUrl;
 }
 
-router.get(`/$/download/:claimName/:claimId`, async ctx => {
+router.get(`/$/api/content/get`, async (ctx) => {
+  const homepage = await fs.readFileSync(path.join(__dirname, '../../custom/content/default.json'), 'utf-8');
+
+  ctx.body = {
+    status: 'success',
+    data: homepage,
+  };
+});
+
+router.get(`/$/download/:claimName/:claimId`, async (ctx) => {
   const streamUrl = getStreamUrl(ctx);
   const downloadUrl = `${streamUrl}?download=1`;
   ctx.redirect(downloadUrl);
 });
 
-router.get(`/$/stream/:claimName/:claimId`, async ctx => {
+router.get(`/$/stream/:claimName/:claimId`, async (ctx) => {
   const streamUrl = getStreamUrl(ctx);
   ctx.redirect(streamUrl);
 });
 
-router.get('*', async ctx => {
+router.get('*', async (ctx) => {
   const html = await getHtml(ctx);
   ctx.body = html;
 });
